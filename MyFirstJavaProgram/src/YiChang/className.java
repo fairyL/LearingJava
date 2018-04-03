e5af8c67ae982922eb182a089cd33a313204effa
fairy <llxaswl@foxmail.com>
2018-04-03 21:28:44 +0800

    元气满满
    字符、异常处理

---
+++ MyFirstJavaProgram/src/YiChang/className.java
@@ -0,0 +1,9 @@
+package YiChang;
+import java.io.*;
+public class className {
+	public void deposit(double amount)throws RemoteException{
+		throw new RemoteException();
+		
+	} 
+	
+}
---
+++ MyFirstJavaProgram/src/zifu/ScannerDemo.java
@@ -0,0 +1,21 @@
+package zifu;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		//从键盘接收数据
+		
+		//next方式接收字符串
+		System.out.println("next方式接收：");
+		//判断是否还有输入
+		if(scan.hasNext()){
+			//使用next方法获取输入的字符串
+			String str1=scan.next();
+			System.out.println("输入的数据为："+str1);
+			
+		}
+		scan.close();
+	}
+}
